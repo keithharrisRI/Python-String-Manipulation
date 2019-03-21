2a4ef59c356518d2e9684e1e35cdea1eb650e75f
Keith Harris <keithharris.student@careerdevs.com>
2019-03-21 04:04:01 +0000

    First commit

---
+++ stringmethods.py
@@ -0,0 +1,13 @@
+str = "This is a sample string"
+
+lower = str.lower()
+upper = str.upper()
+isupper = str.isupper()
+islower = str.islower()
+substr = str[0:8]
+
+print(lower)
+print(upper)
+print(isupper)
+print(islower)
+print(substr)
